5953e14d28d2a58683f43047b71f920d0d20f718
luisma <mrluisgamerps3@gmail.com>
2025-09-20 17:31:58 -0400

    images and more

--- menu/views.py
+++ menu/views.py
@@ -8,8 +8,8 @@ from .forms import ContactForm
 def home(request):
     return render(request, "home.html")
 
-def about(request):
-    return render(request, "about.html")
+def faq(request):
+    return render(request, "faq.html")
 
 
 def contact(request):
